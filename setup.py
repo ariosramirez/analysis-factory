0ee3cf16cad1e8f0f3ce726ca1bb5f4492fe784c
Andres David Rios Ramirez <ariosramirez.data@gmail.com>
2022-02-10 11:42:20 -0300

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='analysis-factory',
+    version='0.0.1',
+    packages=['utils'],
+    url='',
+    license='',
+    author='Andres Rios',
+    author_email='ariosramirez.data@gmail.com',
+    description='Analysis factory repo, how to improve your time to results.'
+)
